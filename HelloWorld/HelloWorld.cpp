a824d83fce972bfa42c1bd9292fabb68eae990b2
ClAndrew <cmlandrews@gmail.com>
2022-12-17 17:40:08 -0800

    Add silly sample

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,15 @@
+// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "Counting:\n";
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << i << "\n";
+    }
+    std::cout << "Done.\n";
+}
